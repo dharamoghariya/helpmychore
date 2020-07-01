c409e1c235505a942e7f98f1fa24d7a012c58a78
Dhara Moghariya <dhararangani76@gmail.com>
2020-07-01 15:51:37 -0400

    Added missing code

---
+++ app/views/base.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, abort
+from jinja2 import TemplateNotFound
+
+main_page = Blueprint('index_page', __name__,
+                        template_folder='templates',
+                        static_folder='static')
+
+@main_page.route('/', defaults={'page': 'index'})
+@main_page.route('/<page>')
+def show(page):
+    try:
+        # return "MAIN PAGE TEST"
+        return render_template('%s.html' % page)
+    except TemplateNotFound:
+        abort(404)
