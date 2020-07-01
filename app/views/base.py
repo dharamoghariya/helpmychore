5ca5c3cc81063e5edac87df442fb03a28fd0de50
ruturajp <ruturajp@sfu.ca>
2020-07-01 19:13:33 -0400

    initial commit frontend

--- app/views/base.py
+++ app/views/base.py
@@ -1,15 +1,16 @@
 from flask import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 
-main_page = Blueprint('index_page', __name__,
-                        template_folder='templates',
-                        static_folder='static')
+main_page = Blueprint(
+    "index_page", __name__, template_folder="templates", static_folder="static"
+)
 
-@main_page.route('/', defaults={'page': 'index'})
-@main_page.route('/<page>')
+
+@main_page.route("/", defaults={"page": "index"})
+@main_page.route("/<page>")
 def show(page):
     try:
         # return "MAIN PAGE TEST"
-        return render_template('%s.html' % page)
+        return render_template("%s.html" % page)
     except TemplateNotFound:
         abort(404)
